4890e55bb57be59245df155c26a51da25d4cde72
tebibux <tebibusolomon79@gmail.com>
2022-08-29 11:44:15 -0700

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     size = len(matrix)
+    if size == 0:
+        print('$')
     for i in range(size):
         iSize = len(matrix[i])
         for j in range(iSize):
             if j != 0:    
                 print(" ", end='')
-            print(matrix[i][j], end='')
+            print("{:d}".format(matrix[i][j]), end='')
             if j == (size - 1):
                 print('', end='$')
         print()
