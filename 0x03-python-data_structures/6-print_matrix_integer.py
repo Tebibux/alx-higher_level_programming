23ae8ea57a8b472542e16832f747aec6c4b683d6
tebibux <tebibusolomon79@gmail.com>
2022-08-29 11:24:20 -0700

    Task 6, The matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    size = len(matrix)
+    for i in range(size):
+        iSize = len(matrix[i])
+        for j in range(iSize):
+            if j != 0:    
+                print(" ", end='')
+            print(matrix[i][j], end='')
+        print()
