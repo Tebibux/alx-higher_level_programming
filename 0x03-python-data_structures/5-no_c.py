c18f12efd46453640f8b2dd90a0c4085636e30e4
tebibux <tebibusolomon79@gmail.com>
2022-08-29 10:36:58 -0700

    Blind c is fixed

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    return ((my_string.upper()).replace("C", ""))
+    no_c_String = ''
+    for i in range(len(my_string)):
+        if (my_string[i] != 'c' and my_string[i] != 'C'):
+            no_c_String += my_string[i]
+    return (no_c_String)
