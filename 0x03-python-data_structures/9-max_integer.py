3bd4077036adfa84fb2a415ac7e8fc6a45fe5e77
tebibux <tebibusolomon79@gmail.com>
2022-08-29 13:21:46 -0700

    Task 9, sort than print the last element

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    for i in range(len(my_list)):
+        for j in range(0, len(my_list) - i -1):
+            if my_list[j] > my_list[j + 1]:
+                temp = my_list[j]
+                my_list[j] = my_list[j + 1]
+                my_list[j + 1] = temp
+    return my_list[-1]
