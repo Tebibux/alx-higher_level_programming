bb7a154679412a8d84c71dd824b2ad010d77f5e9
tebibux <tebibusolomon79@gmail.com>
2022-08-29 12:38:13 -0700

    task 7, addint tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def add_tuple(tuple1=(), tuple2=()):
+    t_1_len = len(tupel1)
+    t_2_len = len(tupel2)
+
+    if t_1_len == 0:
+        a1 = 0
+        a2 = 0
+    elif t_1_len == 1:
+        a1 = tupel1[0]
+        a2 = 0
+    else:
+        a1 = tupel1[0]
+        a2 = tupel1[1]
+
+    if t_2_len == 0:
+        b1 = 0
+        b2 = 0
+    elif t_2_len == 1:
+        b1 = tupel2[0]
+        b2 = 0
+    else:
+        b1 = tupel2[0]
+        b2 = tupel2[1]
+
+    new_tuple = (a1 + b1, a2 + b2)
+
+    return (new_tuple)
