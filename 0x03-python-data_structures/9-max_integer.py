b4ec23b9e4fdfd9d2d8a8b1cc5cb805f62049e26
tebibux <tebibusolomon79@gmail.com>
2022-08-29 13:25:21 -0700

    restriction added

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    for i in range(len(my_list)):
-        for j in range(0, len(my_list) - i -1):
-            if my_list[j] > my_list[j + 1]:
-                temp = my_list[j]
-                my_list[j] = my_list[j + 1]
-                my_list[j + 1] = temp
-    return my_list[-1]
+    if len(my_list) == 0:
+        return 'None'
+    else:
+        for i in range(len(my_list)):
+            for j in range(0, len(my_list) - i -1):
+                if my_list[j] > my_list[j + 1]:
+                    temp = my_list[j]
+                    my_list[j] = my_list[j + 1]
+                    my_list[j + 1] = temp
+        return my_list[-1]
