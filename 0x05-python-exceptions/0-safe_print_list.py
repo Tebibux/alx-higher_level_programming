2bf5fbe23f38a3b77b2daa87785c439b2fd52f69
tebibux <tebibusolomon79@gmail.com>
2022-09-12 13:46:31 -0700

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    nums = 0
+    iteration = 0
     for num in range(x):
         try:
             print("{}".format(my_list[num]), end='')
         except:
             break
         else:
-            nums +=
+            iteration +=
     print()
-    return (nums)
+    return (iteration)
