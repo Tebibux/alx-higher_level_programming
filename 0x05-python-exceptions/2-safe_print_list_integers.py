bd0ac931ebe78996c30b049430be6c952d33a3fa
tebibux <tebibusolomon79@gmail.com>
2022-09-12 14:05:26 -0700

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    iterator = 0
+
+    for num in range(x):
+        try:
+            print("{:d}".format(), end='')
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+        else:
+            iterator += 1
+    print()
+    return(iterator)
