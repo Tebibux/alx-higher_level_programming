130c4084f30e2e88b91de609d6592baa7767f3eb
tebibux <tebibusolomon79@gmail.com>
2022-09-14 12:06:25 -0700

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""define class based on the above"""
+
+
+class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of a squre
+    """
+    def __init__(self, size=0):
+        """initializes the square
+        Args:
+            size (int): size of a side of the square
+        Returns:
+            None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        """calculates the square of an area
+        Returns:
+            The area of the square
+        """
+        return ((self.size)**2)
