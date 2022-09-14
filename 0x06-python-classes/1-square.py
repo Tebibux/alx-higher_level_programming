da4879ab9f184e6bbe85f323c4865fd6d6c09443
tebibux <tebibusolomon79@gmail.com>
2022-09-14 11:37:33 -0700

    fixed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""Square class definition"""
+
+
 class Square:
     """Class Square that defines a square object
+    Attributes:
+        __size (int): size of the side of square
     """
     def __init__(self, size):
         """Initialize method that stores the size of the square
